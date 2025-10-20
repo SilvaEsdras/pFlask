adfbe56fdf1261abeebadf13395ebe0d4b5d5d79
SilvaEsdras <esdrasesportivo@gmail.com>
2025-10-20 15:20:12 -0300

    Implementação do DAO

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
 from flask import Flask, render_template
+from dao.aluno_dao import AlunoDAO
+from dao.professor_dao import ProfessorDAO
+from dao.curso_dao import CursoDAO
 
-# Criação da aplicação Flask.
-app = Flask(__name__)
 
-# Rotas da aplicação e navegação entre páginas.
+app = Flask(__name__) 
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') 
+
+@app.route('/aluno')
+def listar_aluno():
+    dao = AlunoDAO()
+    lista = dao.listar()
+    return render_template('aluno/lista.html', lista_alunos=lista)
+
+@app.route('/professor')
+def listar_professor():
+    dao = ProfessorDAO()
+    lista = dao.listar()
+    return render_template('professor/lista.html', lista_professores=lista)
+
+@app.route('/curso')
+def listar_curso():
+    dao = CursoDAO()
+    lista = dao.listar()
+    return render_template('curso/lista.html', lista_cursos=lista)
 
 @app.route('/sobre')
 def sobre_sistema():
@@ -17,8 +37,5 @@ def ajuda_sistema():
     return render_template('ajuda.html')
 
 
-
-
-#Método 'main' sempre no final do arquivo.
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ dao/professor_dao.py
@@ -0,0 +1,12 @@
+from dao.db_config import get_connection
+
+class ProfessorDAO:
+    sqlSelect = 'SELECT * FROM professor'
+
+    def listar(self):
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(self.sqlSelect)
+        lista = cursor.fetchall()
+        conn.close()
+        return lista
